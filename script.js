dda243aaf80f20d23c0624cea22e4672725ba5bc
YaoDumbledore <95558265+dumbledore96@users.noreply.github.com>
2022-01-09 20:11:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function kick(){
+  let text = document.getElementById("inter").value;
+  if (text == "歐西里斯") {
+    window.alert("答對了");
+    window.location.href="002/002-1.html";
+  } 
+  else {
+    window.alert("答錯了\n給你一個提示:3124");
+  }
+}
